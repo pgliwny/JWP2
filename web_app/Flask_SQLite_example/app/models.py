ee17d390e4a72b7db3bf2ff5aadd9a5571eb7697
Bartłomiej <bartlomiej.runowski@gmail.com>
2024-05-25 15:34:20 +0200

    Adding todo flask app

--- web_app/Flask_SQLite_example/app/models.py
+++ web_app/Flask_SQLite_example/app/models.py
@@ -7,3 +7,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    subject = db.Column(db.String(120), unique=False, nullable=False)
+    time = db.Column(db.String(120), unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<Teacher %r>' % self.name
--- web_app/Flask_SQLite_example/app/routes.py
+++ web_app/Flask_SQLite_example/app/routes.py
@@ -1,11 +1,40 @@
 from flask import render_template, request, redirect, url_for
 from app import app, db
 from app.models import User
+from app.models import Teacher
 
 @app.route('/')
 def index():
     users = User.query.all()
     return render_template('index.html', users=users)
+
+@app.route('/teachers')
+def teacher_index():
+    teachers = Teacher.query.all()
+    return render_template('teacher_index.html', teachers=teachers)
+
+@app.route('/teachers/add', methods=['GET', 'POST'])
+def add_teacher():
+    if request.method == 'POST':
+        name = request.form['name']
+        subject = request.form['subject']
+        if name and subject:
+            new_teacher = Teacher(name=name, subject=subject)
+            db.session.add(new_teacher)
+            db.session.commit()
+            return redirect(url_for('teacher_index'))
+    return render_template('add_user.html')
+
+@app.route('/teachers/delete', methods=['GET', 'POST'])
+def delete_teacher():
+    if request.method == 'POST':
+        teacher_id = request.form['teacher_id']
+        teacher = Teacher.query.get(teacher_id)
+        if teacher:
+            db.session.delete(teacher)
+            db.session.commit()
+            return redirect(url_for('teacher_index'))
+    return render_template('delete_user.html')
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_user():
